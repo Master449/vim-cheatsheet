e1b6f40a290326b4535beda42e3766d34e5bab45
David Flowers II <20436145+Master449@users.noreply.github.com>
2024-06-12 10:09:59 -0500

    Basic reading in logic, just prints out for now

--- main.py
+++ main.py
@@ -11,13 +11,34 @@ if argc == 1 or argc != 2:
 
 inputFile = sys.argv[1]
 
+def read_vim_keybindings(filepath):
+    keybindings = {}
+    try:
+        with open(filepath, 'r') as file:
+            for line in file:
+                line = line.strip()
+                if line.startswith("map") or line.startswith("nmap") or line.startswith("imap"):
+                    parts = line.split()
+                    if len(parts) >= 3:
+                        command = parts[0]
+                        keys = parts[1]
+                        action = ' '.join(parts[2:])
+                        keybindings[keys] = {'command': command, 'action': action}
+    except FileNotFoundError:
+        print("File not found.")
+    except Exception as e:
+        print(f"An error occurred: {e}")
+    return keybindings
+
+
+def read_lua_keybindings():
+    return
+
 if inputFile.endswith(".vim"):
     print("Vim Bindings File passed in.")
-    vimFlag = True;
+    keybindings = read_vim_keybindings(inputFile)
+    print("Key      Command      Action")
+    for keys, details in keybindings.items():
+        print(f"{keys}, {details['command']}, {details['action']}")
 
-def vimFilePresent():
-    return
-
-def luaFilePresent():
-    return
 
