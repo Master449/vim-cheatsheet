6de32fad0b95a3b818f5a6149aa00167cc42e846
David Flowers II <20436145+Master449@users.noreply.github.com>
2024-06-12 09:59:12 -0500

    Added vim configuration found on github, and started args file input python

--- main.py
+++ main.py
@@ -1,5 +1,19 @@
-html = open("template.html).read()
-print(html)
+import sys
+
+argc = len(sys.argv)
+vimFlag = False
+
+if argc == 1 or argc != 2:
+    print("Usage:")
+    print("    python3 main.py /path/to/bindings.vim")
+    print("    python3 main.py /path/to/bindings.lua")
+    quit()
+
+inputFile = sys.argv[1]
+
+if inputFile.endswith(".vim"):
+    print("Vim Bindings File passed in.")
+    vimFlag = True;
 
 def vimFilePresent():
     return
